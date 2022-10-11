2afa61a798467ef18ef75f9c7c8fb34ba0f12f3b
Stephen Ierodiaconou <stevegeek@gmail.com>
2022-10-11 08:11:31 +0200

    first commit

---
+++ lib/encoded_id/rails/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module EncodedId
+  module Rails
+    VERSION = "0.1.0"
+  end
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "encoded_id/rails"
+
+require "minitest/autorun"
