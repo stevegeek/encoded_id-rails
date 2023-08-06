e9b23cf4866047bf80fae9a9df94abe5857a2512
Stephen Ierodiaconou <stevegeek@gmail.com>
2023-08-06 22:47:06 +0200

    to_param should raise rather than return nil

--- lib/encoded_id/rails/path_param.rb
+++ lib/encoded_id/rails/path_param.rb
@@ -7,7 +7,7 @@ module EncodedId
   module Rails
     module PathParam
       def to_param
-        encoded_id
+        encoded_id || raise(StandardError, "Cannot create path param for #{self.class.name} without an encoded id")
       end
     end
   end
--- lib/encoded_id/rails/slugged_path_param.rb
+++ lib/encoded_id/rails/slugged_path_param.rb
@@ -7,7 +7,7 @@ module EncodedId
   module Rails
     module SluggedPathParam
       def to_param
-        slugged_encoded_id
+        slugged_encoded_id || raise(StandardError, "Cannot create path param for #{self.class.name} without an encoded id")
       end
     end
   end
