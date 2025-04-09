bc68e504c2ffb94d28e00d8a0caaea51f8fb8763
Stephen Ierodiaconou <stevegeek@gmail.com>
2025-04-09 23:45:46 +0200

    It was possible to modify a models id and the instance still retain the memoized encoded_id, this change is meant to make this more robust

--- lib/encoded_id/rails/model.rb
+++ lib/encoded_id/rails/model.rb
@@ -17,30 +17,60 @@ module EncodedId
         end
       end
 
+      attr_accessor :encoded_id_memoized_with_id
+
+      def clear_encoded_id_cache!
+        [:@encoded_id_hash, :@encoded_id, :@slugged_encoded_id].each do |var|
+          remove_instance_variable(var) if instance_variable_defined?(var)
+        end
+        self.encoded_id_memoized_with_id = nil
+      end
+
+      def check_and_clear_memoization
+        clear_encoded_id_cache! if encoded_id_memoized_with_id && encoded_id_memoized_with_id != id
+      end
+      
       def encoded_id_hash
         return unless id
-        return @encoded_id_hash if defined?(@encoded_id_hash) && !id_changed?
-        self.class.encode_encoded_id(id)
+        check_and_clear_memoization
+        return @encoded_id_hash if defined?(@encoded_id_hash)
+        
+        self.encoded_id_memoized_with_id = id
+        @encoded_id_hash = self.class.encode_encoded_id(id)
       end
 
       def encoded_id
         return unless id
-        return @encoded_id if defined?(@encoded_id) && !id_changed?
+        check_and_clear_memoization
+        return @encoded_id if defined?(@encoded_id)
+        
         encoded = encoded_id_hash
         annotated_by = EncodedId::Rails.configuration.annotation_method_name
         return @encoded_id = encoded unless annotated_by && encoded
+        
         separator = EncodedId::Rails.configuration.annotated_id_separator
+        self.encoded_id_memoized_with_id = id
         @encoded_id = EncodedId::Rails::AnnotatedId.new(id_part: encoded, annotation: send(annotated_by.to_s), separator: separator).annotated_id
       end
 
       def slugged_encoded_id
         return unless id
-        return @slugged_encoded_id if defined?(@slugged_encoded_id) && !id_changed?
+        check_and_clear_memoization
+        return @slugged_encoded_id if defined?(@slugged_encoded_id)
+        
         with = EncodedId::Rails.configuration.slug_value_method_name
         separator = EncodedId::Rails.configuration.slugged_id_separator
         encoded = encoded_id
         return unless encoded
+        
+        self.encoded_id_memoized_with_id = id
         @slugged_encoded_id = EncodedId::Rails::SluggedId.new(id_part: encoded, slug_part: send(with.to_s), separator: separator).slugged_id
+      end
+      
+      def reload(options = nil)
+        result = super
+        clear_encoded_id_cache!
+        result
       end
 
       # By default the annotation is the model name (it will be parameterized)
@@ -57,11 +87,10 @@ module EncodedId
         raise StandardError, "You must define a method to generate the slug for the encoded ID of #{self.class.name}"
       end
 
-      # When duplicating an ActiveRecord object, we want to reset the memoized encoded_id
+      # When duplicating an ActiveRecord object, we want to reset the memoized encoded IDs
       def dup
         super.tap do |new_record|
-          new_record.send(:remove_instance_variable, :@encoded_id) if new_record.instance_variable_defined?(:@encoded_id)
-          new_record.send(:remove_instance_variable, :@slugged_encoded_id) if new_record.instance_variable_defined?(:@slugged_encoded_id)
+          new_record.clear_encoded_id_cache!
         end
       end
     end
